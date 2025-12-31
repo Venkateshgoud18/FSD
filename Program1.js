1d4cea0e37915b7f1d514e481738e45db9f2611e
Venkateshgoud18 <goudvenki5@gmail.com>
2025-12-31 04:16:09 +0000

    arr functions commit

---
+++ Program1.js
@@ -0,0 +1,8 @@
+let arr=[1,2,3,4,5];
+
+console.log("The arrays doubled values :");
+let a=arr.forEach(n=>n+n);
+console.log(a);
+console.log("THe filtered values");
+let b=arr.filter(n=>n>20);
+console.log(b);
